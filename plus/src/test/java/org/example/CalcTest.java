48831be0a63c6fbcae38b41652de5f5388733c90
Rina1404 <arina14042005@mail.ru>
2025-06-23 17:13:38 +0000

    arina_plus_newcatalog

---
+++ plus/src/test/java/org/example/CalcTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class CalcTest {
+
+    public boolean areDoubleEqual(double a, double b, double epsilon) {
+        return Math.abs(a - b) <= epsilon;
+    }
+@Test
+    public void addTest() {
+        double value1 = 5.0 + 4.0;
+        double value2 = 9.0;
+        boolean b = areDoubleEqual(value1,value2, 1e-10);
+        assertTrue ("Значения близки к друг другу", b);
+    }
+}
+
+
